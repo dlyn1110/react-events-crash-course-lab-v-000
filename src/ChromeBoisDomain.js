a888507502a67b54fdd0d2f2cc6e252d381a5439
Dani Lyn <dlyn1110@gmail.com>
2019-04-23 01:58:25 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -14,7 +14,9 @@ export default class ChromeBoisDomain extends Component {
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
   }
-
+    handleClick = (event) => {
+      toggleCycling();
+    }
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
